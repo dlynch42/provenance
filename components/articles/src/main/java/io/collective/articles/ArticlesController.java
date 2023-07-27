59d5f29965fd8757599a3b319fd97da5d9b9a534
Devin Lynch <djlynch17@gmail.com>
2023-07-27 10:55:15 -0700

    Test passed for ArticlesController

--- components/articles/src/main/java/io/collective/articles/ArticlesController.java
+++ components/articles/src/main/java/io/collective/articles/ArticlesController.java
@@ -6,6 +6,8 @@ import org.eclipse.jetty.server.Request;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArticlesController extends BasicHandler {
@@ -16,19 +18,40 @@ public class ArticlesController extends BasicHandler {
         this.gateway = gateway;
     }
 
+    // Handles incoming HTTP request for specified endpoint
     @Override
     public void handle(String target, Request request, HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
+        // Articles endpoint
         get("/articles", List.of("application/json", "text/html"), request, servletResponse, () -> {
 
             { // todo - query the articles gateway for *all* articles, map record to infos, and send back a collection of article infos
+                // Create a list to store ArticleInfo objects
+                List<ArticleInfo> articles = new ArrayList<>();
 
+                // Iterate through all article records
+                gateway.findAll().forEach(article -> 
+                    // Map each record to an articleInfo object
+                    articles.add(new ArticleInfo(article.getId(), article.getTitle())));
+                
+                // Write the articleInfo list using the given JSON response body
+                writeJsonBody(servletResponse, articles);
             }
         });
 
+        // Available endpoint; pretty much the same as above with a different endpoint and only looking for available articles
         get("/available", List.of("application/json"), request, servletResponse, () -> {
 
             { // todo - query the articles gateway for *available* articles, map records to infos, and send back a collection of article infos
+                // Create a list to store ArticleInfo objects
+                List<ArticleInfo> articles = new ArrayList<>();
 
+                // Iterate through all article records
+                gateway.findAvailable().forEach(article -> // Changed function from findAll to findAvailable
+                    // Map each record to an articleInfo object
+                    articles.add(new ArticleInfo(article.getId(), article.getTitle())));
+                
+                // Write the articleInfo list using the given JSON response body
+                writeJsonBody(servletResponse, articles);
             }
         });
     }
